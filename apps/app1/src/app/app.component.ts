65565bf930bd474dc5ca6642b5ca3f65f03b9b8a
Altan Stalker <ci@nrwl.io>
2022-09-08 19:48:10 -0400

    Initial commit

---
+++ apps/app1/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'unai-repro-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'app1';
+}
