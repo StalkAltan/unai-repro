6626db65a7f22560aa5006e833469d9c1cd6ed27
Altan Stalker <ci@nrwl.io>
2022-09-08 20:13:17 -0400

    project/executor setup

---
+++ libs/test/src/executors/build/executor.ts
@@ -0,0 +1,40 @@
+import { BuildExecutorSchema } from './schema';
+import { ExecutorContext } from "nx/src/config/misc-interfaces";
+import { detectPackageManager } from "nx/src/utils/package-manager";
+import { spawn } from "child_process";
+
+export default async function runExecutor(options: BuildExecutorSchema, context: ExecutorContext) {
+    const packageManager = detectPackageManager();
+    const packageManagerCmd =
+    packageManager === 'pnpm'
+      ? 'pnpx'
+      : packageManager === 'yarn'
+      ? 'yarn'
+      : 'npx'
+
+  const libRoot = context.workspace.projects[context.projectName!!].root;
+    console.log("LIBROOT", libRoot);
+
+    const executionCode = await new Promise((res) => {
+      const args = [
+          'tsc',
+          '-p',
+          `./${libRoot}/tsconfig.${libRoot.includes('apps') ? 'app' : 'lib'}.json`,
+          '--noEmit',
+          'true'
+        ]
+      console.log(args.join(' '));
+
+      const child = spawn(
+        packageManagerCmd,
+        args,
+        {
+          stdio: 'inherit',
+        }
+      )
+      child.on('data', (args) => console.log(args));
+      child.on('close', (code) => res(code))
+    })
+
+  return { success: executionCode === 0 }
+}
